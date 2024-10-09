d38b71168688a671454c81b486661c707d65f66f
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-10-09 16:21:45 +0200

    Too tired

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,19 @@ function App() {
   return (
     <>
       <div className="header">
-        <button>Nav buttons</button>
-        <button>Nav buttons</button>
-        <button>Nav buttons</button>
-        <button>Nav buttons</button>
+        <button className="navBtn">Nav buttons</button>
+        <button className="navBtn">Nav buttons</button>
+        <button className="navBtn">Nav buttons</button>
+        <button className="navBtn">Nav buttons</button>
       </div>
       <div className="mainCont">
-        <div className="aboutMeDiv">About me div</div>
+        <div className="aboutMeDivContainer">
+          <div className="aboutMeHeader">About me div</div>
+          <div className="aboutMeMain">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam,
+            voluptas consequatur quae ducimus corporis soluta.
+          </div>
+        </div>
         <div className="myProjectsDiv">My projects go here</div>
         <div className="mySkills">My skills go here</div>
         <div className="contactMeDiv">Contact details</div>
