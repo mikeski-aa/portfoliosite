b17834bf6bccfa0cb41e02c7085c1e3109c82aa7
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-10-10 15:16:05 +0200

    General site layout created

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,4 +1,6 @@
 import "../styles/contactme.css";
+import linIcon from "../assets/icons/linkedin.svg";
+import emailIcon from "../assets/icons/email.svg";
 
 function ContactMe() {
   return (
@@ -6,7 +8,16 @@ function ContactMe() {
       <div className="contactMeTitle">
         <h2>Contact me</h2>
       </div>
-      <div className="contactMeDiv"></div>
+      <div className="contactMeDiv">
+        <button className="contactOption">
+          <img src={linIcon} className="contactIcon"></img>
+          LinkedIn
+        </button>
+        <button className="contactOption">
+          <img src={emailIcon} className="contactIcon"></img>
+          Email
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/MySkills.jsx
+++ src/components/MySkills.jsx
@@ -1,5 +1,11 @@
-import profilePic from "../assets/placeholderprof.jpg";
 import "../styles/myskills.css";
+import cssIcon from "../assets/icons/css.svg";
+import htmlIcon from "../assets/icons/html.svg";
+import jsIcon from "../assets/icons/js.svg";
+import mongoIcon from "../assets/icons/mongo.svg";
+import nodeIcon from "../assets/icons/node.svg";
+import psqlIcon from "../assets/icons/psql.svg";
+import reactIcon from "../assets/icons/react.svg";
 
 function MySkills() {
   return (
@@ -7,7 +13,36 @@ function MySkills() {
       <div className="mySkillsTitle">
         <h2>My skills</h2>
       </div>
-      <div className="mySkillsDiv"></div>
+      <div className="mySkillsDiv">
+        <div className="skillBox">
+          <img src={jsIcon} className="skillIcon"></img>
+          <div className="heading">Javascript</div>
+        </div>
+        <div className="skillBox">
+          <img src={reactIcon} className="skillIcon"></img>
+          <div className="heading">React</div>
+        </div>
+        <div className="skillBox">
+          <img src={nodeIcon} className="skillIcon"></img>
+          <div className="heading">Node.js</div>
+        </div>
+        <div className="skillBox">
+          <img src={psqlIcon} className="skillIcon"></img>
+          <div className="heading">PostgreSQL</div>
+        </div>
+        <div className="skillBox">
+          <img src={mongoIcon} className="skillIcon"></img>
+          <div className="heading">MongoDB</div>
+        </div>
+        <div className="skillBox">
+          <img src={cssIcon} className="skillIcon"></img>
+          <div className="heading">CSS</div>
+        </div>
+        <div className="skillBox">
+          <img src={htmlIcon} className="skillIcon"></img>
+          <div className="heading">HTML</div>
+        </div>
+      </div>
     </div>
   );
 }
