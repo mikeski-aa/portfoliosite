2fcc293a1718b9e3a3e2c1713a4704bb9276476c
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-10-12 18:01:22 +0200

    Vert bar working, now adding explorer

--- src/components/VertBar.jsx
+++ src/components/VertBar.jsx
@@ -3,22 +3,37 @@ import FilesIcon from "../assets/bwicons/filealt.svg?react";
 import GithubIcon from "../assets/bwicons/github2.svg?react";
 import LinkedIn from "../assets/bwicons/linkedin.svg?react";
 import Email from "../assets/bwicons/email.svg?react";
+import { useState } from "react";
 
 function VertBar() {
+  const [showExp, setShowExp] = useState(false);
+
+  const handleExplorerClick = () => {
+    if (showExp) {
+      setShowExp(false);
+    } else {
+      setShowExp(true);
+    }
+  };
   return (
     <div className="vertBar">
-      <button className="vertBtn">
-        <FilesIcon className="vertIcon" />
-      </button>
-      <button className="vertBtn">
-        <GithubIcon className="vertIcon" />
-      </button>
-      <button className="vertBtn">
-        <LinkedIn className="vertIcon" />
-      </button>
-      <button className="vertBtn">
-        <Email className="vertIcon" />
-      </button>
+      <div className="vertBtnDiv">
+        <button className={`vertBtn ${showExp}`} onClick={handleExplorerClick}>
+          <FilesIcon className={`vertIcon ${showExp}`} />
+        </button>
+        <button className="vertBtn">
+          <GithubIcon className="vertIcon" />
+        </button>
+        <button className="vertBtn">
+          <LinkedIn className="vertIcon" />
+        </button>
+        <button className="vertBtn">
+          <Email className="vertIcon" />
+        </button>
+      </div>
+      <div className={`explorerDiv ${showExp}`}>
+        ASDASDADASDASDasdasdasdasdasdasdadasdasdasd asdasdasdasdasd
+      </div>
     </div>
   );
 }
