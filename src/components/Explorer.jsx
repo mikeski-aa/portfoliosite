1e584e052d5afd6e9c078a7ba7fbe11b6ddf8026
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-10-12 18:51:34 +0200

    Heretical code was written here

--- src/components/Explorer.jsx
+++ src/components/Explorer.jsx
@@ -6,9 +6,11 @@ function Explorer() {
     <div className="explorerContainer">
       <div className="explorerHeading">Explorer</div>
       <div className="explorerContent">
-        <Folder name="PORTFOLIOSITE [WSL:UBUNTU]" />
-        <Folder name="src" />
-        <Folder name="components" />
+        <Folder
+          rootName="PORTFOLIOSITE [WSL:UBUNTU]"
+          childOne="src"
+          childTwo="components"
+        />
       </div>
     </div>
   );
--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -2,28 +2,53 @@ import { useState } from "react";
 import Arrow from "../assets/arrow.svg?react";
 
 function Folder(props) {
-  const [folderStat, setFolderStat] = useState(false);
-  const handleFolderClick = () => {
-    if (folderStat) {
+  const [rootStat, setFolderStat] = useState(false);
+  const [childOneStat, setChildOneStat] = useState(false);
+  const [childTwoStat, setChildTwoStat] = useState(false);
+
+  const handleOuterClick = () => {
+    if (rootStat) {
       setFolderStat(false);
     } else {
       setFolderStat(true);
     }
   };
 
+  const handleChildOne = () => {
+    if (childOneStat) {
+      setChildOneStat(false);
+    } else {
+      setChildOneStat(true);
+    }
+  };
+
+  const handleChildTwo = () => {
+    if (childTwoStat) {
+      setChildTwoStat(false);
+    } else {
+      setChildTwoStat(true);
+    }
+  };
+
   // I am thinking this component could be re-written using recursive components
   return (
     <div className="folderDiv">
-      <button className="folderBtn" onClick={handleFolderClick}>
-        <Arrow className={`folderChevron ${folderStat}`} />
-        {props.name}
+      <button className="folderBtn" onClick={handleOuterClick}>
+        <Arrow className={`folderChevron ${rootStat}`} />
+        {props.rootName}
       </button>
-      <div className={`fileList ${folderStat}`}>
-        <button className="folderBtn" onClick={handleFolderClick}>
-          <Arrow className={`folderChevron ${folderStat}`} />
-          {props.name}
+      <div className={`fileList ${rootStat} childOne`}>
+        <button className="folderBtn btnOne" onClick={handleChildOne}>
+          <Arrow className={`folderChevron ${childOneStat}`} />
+          {props.childOne}
         </button>
-        <div className={`fileList ${folderStat}`}></div>
+        <div className={`fileList ${childOneStat} childTwo`}>
+          <button className="folderBtn btnTwo" onClick={handleChildTwo}>
+            <Arrow className={`folderChevron ${childTwoStat}`} />
+            {props.childTwo}
+          </button>
+          <div className={`fileList ${childTwoStat}`}></div>
+        </div>
       </div>
     </div>
   );
