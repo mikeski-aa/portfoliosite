ec526285633203b61e7d2c85f9f841889d1247ef
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-10-13 01:53:51 +0200

    Footer edited and working

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,17 @@
 import "../styles/footer.css";
+import Remote from "../assets/bwicons/remote.svg?react";
+import Branch from "../assets/bwicons/branch.svg?react";
 
 function Footer() {
   return (
     <div className="footer">
-      <div className="leftPart">WSL Ubuntu</div>
-      <div className="middlePart">Main</div>
-      <div className="rightPart">{`{} JavaScript JSX`}</div>
+      <div className="leftPart">
+        <Remote className="footerIcon" /> WSL Ubuntu
+      </div>
+      <div className="middlePart">
+        <Branch className="footerIcon" /> Main
+      </div>
+      <div className="rightPart">{`{ } JavaScript JSX`}</div>
     </div>
   );
 }
