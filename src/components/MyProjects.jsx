8a0aaa23176d2040401dd0d66a78cb3fb308e0e4
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-10-12 11:15:31 +0200

    Working on styling projects

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -6,6 +6,28 @@ import MessengerCarousel from "./MessengerCarousel";
 function MyProjects() {
   return (
     <div className="myProjectsComponent">
+      <div className="codeStyle">
+        <div className="line">
+          import <span className="fileStyle">OdinbookCarousel</span> from{" "}
+          <span className="linkStyle">"./OdinbookCarousel";</span>
+        </div>
+        <div className="line">
+          import <span className="linkStyle">"../styles/myprojects.css";</span>
+        </div>
+        <br></br>
+        <div className="line">
+          <span className="fcnStyle">function</span>{" "}
+          <span className="nameStyle">AboutMe</span>
+          <span className="brackStyle">
+            {"("}
+            <span className="fileStyle">props</span>
+            {") {"}
+          </span>
+        </div>
+        <div>...</div>
+
+        <div className="line">&nbsp;&nbsp;&nbsp;{`return (`}</div>
+      </div>
       <div className="myProjectsTitle">
         <h2>My projects</h2>
       </div>
