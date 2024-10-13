1d3ce26928ec898aa80b517e7e06ca0eca40b2f6
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-10-13 23:03:54 +0200

    Creating interactable terminal

--- src/components/TerminalBox.jsx
+++ src/components/TerminalBox.jsx
@@ -1,10 +1,11 @@
 import "../styles/terminalbox.css";
 import BracketIcon from "../assets/terminalicons/brackets.svg?react";
 import TrashIcon from "../assets/terminalicons/trash.svg?react";
-import CubeIcon from "../assets/terminalicons/cubemoved.svg?react";
+import CubeIcon from "../assets/terminalicons/cube-svgrepo-com.svg?react";
 import EllipsisIcon from "../assets/terminalicons/ellipsis.svg?react";
-import CrossIcon from "../assets/icons/cross.svg?react";
+import PlusIcon from "../assets/terminalicons/pluslargegrey.svg?react";
 import ArrowIcon from "../assets/arrow.svg?react";
+import CrossIcon from "../assets/bwicons/cross2.svg?react";
 
 function TerminalBox(props) {
   return (
@@ -18,7 +19,29 @@ function TerminalBox(props) {
           <button className="terminalBtn">PORTS</button>
         </div>
         <div className="terminalRightBtnDiv">
-          <CubeIcon className="terminalIcon" />
+          <button className="terminalBtnDummy">
+            <CubeIcon className="terminalIcon" /> bash - portfoliosite
+          </button>
+          <div className="terminalBtnDummyDuoDiv">
+            <button className="terminalBtnDummyDuo">
+              <PlusIcon className="terminalIcon" />
+            </button>
+            <button className="terminalBtnDummyDuo">
+              <ArrowIcon className="terminalIcon down" />
+            </button>
+          </div>
+          <button className="terminalBtnDummy">
+            <TrashIcon className="terminalIcon" />
+          </button>
+          <button className="terminalBtnDummy">
+            <EllipsisIcon className="terminalIcon" />
+          </button>
+          <button className="terminalBtnDummy">
+            <ArrowIcon className="terminalIcon top" />
+          </button>
+          <button className="terminalBtnDummy">
+            <CrossIcon className="terminalIcon invert" />
+          </button>
         </div>
       </div>
       <div className="mainTerminalBox">asdasdasdasd</div>
